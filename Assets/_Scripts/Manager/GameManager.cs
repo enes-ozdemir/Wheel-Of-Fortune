0c4b78bb109b02a014439a24f7ec7bac8cb9262e
Enes Özdemir <enesozdemir6167@hotmail.com>
2023-03-01 16:55:07 +0300

    GameManager added

---
+++ Assets/_Scripts/Manager/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using _Scripts.UI;
+using UnityEngine;
+
+namespace _Scripts.Manager
+{
+    public class GameManager : MonoBehaviour
+    {
+        private int _gameLevel = 1;
+        [SerializeField] private int zoneCount = 30;
+        public Action onLevelCompleted;
+        [SerializeField] private ZoneController zoneController;
+
+        private void OnEnable()
+        {
+            onLevelCompleted += IncreaseLevel;
+        }
+        
+        private void OnDisable()
+        {
+            onLevelCompleted -= IncreaseLevel;
+        }
+
+        private void Awake()
+        {
+            _gameLevel = 1;
+            zoneController.SetZones(zoneCount);
+        }
+
+        private void IncreaseLevel()
+        {
+            _gameLevel++;
+        }
+
+    }
+}
